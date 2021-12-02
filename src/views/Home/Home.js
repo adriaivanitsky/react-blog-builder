4d58e55c1d314d2d1a51f5e486b3aef00a45daeb
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-12-01 16:32:10 -0800

    got the editor and preview to share state for title and subtitle inputs

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import { useState } from 'react';
 import Preview from '../../components/Preview/Preview';
 import Editor from '../../components/Editor/Editor';
 
@@ -7,12 +6,17 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [font, setFont] = useState('fredoka');
+  const [align, setAlign] = useState('center');
+  const [text, setText] = useState('');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} subtitle={subtitle}/>
+      {/* <Editor title={title} setTitle={setTitle}/> */}
+      <Editor {...{ title, setTitle, subtitle, setSubtitle, font, setFont, align, setAlign, text, setText }}/>
     </main>
   );
 }
